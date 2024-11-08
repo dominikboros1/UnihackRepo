5da634156b3863e4c6fca9f68cee562dc457fd98
Dodo - Laptop <dominikboros1@gmail.com>
2024-11-08 19:28:09 +0200

    Map Markers Beta

---
+++ assets/markers.ts
@@ -0,0 +1,109 @@
+export const markers = [
+    // Kaufland - Red
+    {
+        id: '1',
+        latitude: 45.733148,
+        longitude: 21.230227,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+        name: 'Kaufland Calea Martirilor',
+        color: 'red'
+    },
+    {
+        id: '1',
+        latitude: 45.747138,
+        longitude: 21.236931,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+        name: 'Kaufland Gheorghe Lazăr',
+        color: 'red'
+    },
+    {
+        latitude: 45.738785,
+        longitude: 21.231034,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+        name: 'Kaufland Circumvalatiunii',
+        color: 'red'
+    },
+
+    // Lidl - Blue
+    {
+        id: '2',
+        latitude: 45.745617,
+        longitude: 21.228378,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+        name: 'Lidl Dr. Iosif Bulbuca',
+        color: '#133E87'
+    },
+    {
+        id: '2',
+        latitude: 45.755213,
+        longitude: 21.232104,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+        name: 'Lidl Gheorghe Lazăr',
+        color: '#133E87'
+    },
+
+    // Profi - Green
+    {
+        id: '3',
+        latitude: 45.743497,
+        longitude: 21.243185,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+        name: 'Profi Standard Andrei Saguna',
+        color: '#F95454'
+    },
+    {
+        id: '3',
+        latitude: 45.754213,
+        longitude: 21.230965,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+        name: 'Profi Go Studentilor',
+        color: '#F95454'
+    },
+
+    // Penny - Yellow
+    {
+        id: '4',
+        latitude: 45.756582,
+        longitude: 21.215987,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+        name: 'Penny Timisoara Sud',
+        color: 'yellow'
+    },
+    {
+        id: '4',
+        latitude: 45.745347,
+        longitude: 21.210456,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+        name: 'Penny Calea Aradului',
+        color: 'yellow'
+    },
+
+    // Carrefour - Orange
+    {
+        id: '5',
+        latitude: 45.753100,
+        longitude: 21.223990,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+        name: 'Carrefour Calea Șagului',
+        color: 'orange'
+    },
+    {
+        id: '5',
+        latitude: 45.741786,
+        longitude: 21.215738,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+        name: 'Carrefour Torontalului',
+        color: 'orange'
+    }
+];
