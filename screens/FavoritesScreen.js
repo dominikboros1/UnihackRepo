fc7caf91cc2e4c8b519ffff5309da44f31258dbf
Dodo - Laptop <dominikboros1@gmail.com>
2024-11-10 12:45:39 +0200

    Final Favorite

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,70 @@
-import React from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import React from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity, Alert,
+} from 'react-native';
+import markers from "../functions/markers"
 
 const FavoritesScreen = ({ favorites }) => {
+
+    const Store = (props) => {
+        return (
+            <TouchableOpacity onPress={() => Alert.alert(props.text + " " + "Selected")} style={styles.TagContainer}>
+                <View style={styles.item}>
+                    <View style={styles.itemleft}>
+                        <View style={styles.square}></View>
+                        <Text style={styles.itemText}>{props.text}</Text>
+                    </View>
+                    <Text style={styles.circular}></Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+    const styles = StyleSheet.create({
+        item:{
+            backgroundColor: 'lightgrey',
+            padding: 15,
+            borderRadius: 15,
+            width: 750,
+            marginHorizontal: 20,
+            flexDirection:'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            marginBottom: 30,
+        },
+        itemleft:{
+            flexDirection: "row",
+            alignItems: "center",
+            flexWrap: 'wrap',
+        },
+        square:{
+            width: 24,
+            height: 24,
+            backgroundColor:'red',
+            opacity: 0.6,
+            borderRadius: 5,
+            marginRight: 15,
+        },
+        itemText:{
+            maxWidth: 100,
+            fontWeight: "bold",
+
+        },
+        circular:{
+            width: 24,
+            height: 24,
+            backgroundColor:'#f94a87',
+            opacity: 0.4,
+            borderRadius: 5,
+            marginRight: 15,
+        },
+        TagContainer: {
+            alignItems: 'center',
+        },
+
+    });
 
     const favStyles = StyleSheet.create({
         favoritesContainer: {
@@ -32,21 +95,39 @@ const FavoritesScreen = ({ favorites }) => {
         },
     });
 
+    const stylesFav = StyleSheet.create({
+        container:{
+            flex: 1,
+        },
+        tasksWrapper:{
+            padding: 80,
+            paddingHorizontal: 20,
+
+        },
+        selectionTitle:{
+            fontSize: 50,
+            fontWeight: 'bold',
+            color: '#F8E559',
+        },
+        items:{
+            marginTop: 30,
+        },
+        backgroundImage:{
+            flex: 1,
+        }
+    });
+
+
     return (
-        <View style={favStyles.favoritesContainer}>
-            {favorites && favorites.length > 0 ? (
-                <FlatList
-                    data={favorites}
-                    keyExtractor={(item) => item.id}
-                    renderItem={({ item }) => (
-                        <View style={favStyles.item}>
-                            <Text style={favStyles.itemText}>{item.value}</Text>
-                        </View>
-                    )}
-                />
-            ) : (
-                <Text style={favStyles.emptyText}>Lista de favorite este goală</Text>
-            )}
+        <View style={stylesFav.container}>
+            <View style={stylesFav.tasksWrapper}>
+                <View style={stylesFav.items}>
+                    <Store text = {'Kaufland'} />
+                    <Store text = {'Lidl'} />
+                    <Store text = {'Penny'} />
+                    <Store text = {'Carrefour'} />
+                </View>
+            </View>
         </View>
     );
 };
